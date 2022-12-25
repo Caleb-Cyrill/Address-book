24d92527ff880ddd71513ca19ae5c1a365e10dfa
CalebCyrill <caleb.cyrill@gmail.com>
2022-12-25 09:54:19 -0600

    Removed unnecessary imports and scripts for bootstrap and updated route aliases

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :phone_numbers
+  resources :phone_numbers, path: :phone
   resources :emails
-  resources :addresses
+  resources :addresses, path: :address
   resources :people
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
